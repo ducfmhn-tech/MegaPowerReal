6a6c7237c3559f42834b06cb0d502b06218103f6
ducfmhn-tech <ducfm.hn@gmail.com>
2025-11-17 13:55:21 +0700

    Update logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -1,6 +1,16 @@
-# utils/logger.py
 import datetime,sys
 def log(msg):
+    """
+    Ghi một thông điệp có timestamp (UTC) ra console và flush buffer ngay lập tức.
+    
+    Args:
+        msg (str): Nội dung thông điệp.
+    """
+    # Lấy thời gian hiện tại theo múi giờ UTC
     t = datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
+    
+    # In thông điệp
     print(f"[{t}] {msg}")
+    
+    # Flush buffer để đảm bảo thông điệp xuất hiện ngay lập tức
     sys.stdout.flush()
